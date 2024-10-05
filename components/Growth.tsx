9df2ffd55148333d09798be208a0aba779c08aad
victormeudic <victormeudic@gmail.com>
2024-10-05 10:39:32 +0200

    Fix bug for deployment

--- components/Growth.tsx
+++ components/Growth.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import React, { useRef } from "react";
+import React from "react";
 import Percentage from "./Percentage";
 import Button from "./Button";
 import { motion } from "framer-motion";
